1b2c06f5b34caafa5e6ddaa2adf1483530fd6806
Zuzana Chlupackova <zuzanqa@users.noreply.github.com>
2024-10-07 21:12:04 +0200

    Update http-server.ts

--- src/interfaces/http-server.ts
+++ src/interfaces/http-server.ts
@@ -1,13 +1,13 @@
 /**
- * Desribes coordinates of arbitrary http server
+ * Desribes the coordinates of an arbitrary HTTP server
  */
 export interface HttpServer {
   /** Name of the host (FQDN or IP) **/
   hostname: string;
 
-  /** Port where to contact server. If leaved empty 80 or 443 will be used based on the value of teh secure parameter **/
+  /** The port at which the server should be contacted. If left empty, 80 or 443 will be used, based on the value of the secure parameter **/
   port?: number;
 
-  /**Ttrue, if the server shall use https instead of http **/
+  /** True, if the server should use HTTPS instead of HTTP **/
   secure: boolean;
 }
